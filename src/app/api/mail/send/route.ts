b4aa39cb6d85531c62fa857c0ae306e5c1706ad6
kojikawazu <nanairo7.daydream21@gmail.com>
2024-02-28 00:25:29 +0900

    定数化

--- src/app/api/mail/send/route.ts
+++ src/app/api/mail/send/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { ALLOWED_ORIGINS } from "@/app/utils/constants";
 import Mailjet from "node-mailjet";
 
 const mailjet = new Mailjet({
@@ -6,12 +6,12 @@ const mailjet = new Mailjet({
     apiSecret: process.env.MAILJET_SECRET_KEY
 });
 
-// 許可するオリジンのリスト
-const allowedOrigins = [
-    process.env.ORIGIN_DOMAIN_L,
-    process.env.ORIGIN_DOMAIN_WEB,
-];
-
+/**
+ * メール送信
+ * @param request 
+ * @param response 
+ * @returns 
+ */
 export async function POST(
     request: Request,
     response: Response
@@ -19,7 +19,7 @@ export async function POST(
     const origin = request.headers.get("origin") || '';
 
     // オリジンが許可されているかチェック
-    if (!allowedOrigins.includes(origin)) {
+    if (!ALLOWED_ORIGINS.includes(origin)) {
         // 許可されていないオリジンからのリクエストの場合
         return new Response(JSON.stringify({ message: "Forbidden" }), {
             status: 403,
@@ -62,7 +62,7 @@ export async function POST(
         });
     } catch (err) {
         console.error(err);
-        return new Response(JSON.stringify(err), {
+        return new Response(JSON.stringify({ message: "Internal Server Error" }), {
             status: 500,
             headers: {
                 'Content-Type': 'application/json'
---
+++ src/app/utils/constants.ts
@@ -0,0 +1,6 @@
+
+// 許可するオリジンのリスト
+export const ALLOWED_ORIGINS = [
+    process.env.ORIGIN_DOMAIN_L,
+    process.env.ORIGIN_DOMAIN_WEB,
+];
